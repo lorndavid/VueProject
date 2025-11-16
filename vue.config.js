e28fae94de6d3b4e8c0ad335dd2bf844abe3be68
Lorn David <lorndavid@github.com>
2025-11-16 11:35:05 +0700

    Deploy app to docs folder for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/VueProject/'
-    : '/'
+    : '/',
+  outputDir: path.resolve(__dirname, 'docs'),
+  filenameHashing: true
 })
